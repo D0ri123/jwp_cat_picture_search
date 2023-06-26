1550536b621b414a5e6f98e9cf5408879c70939c
D0ri123 <lsh81550@gmail.com>
2023-06-26 15:30:54 +0900

    setting: entity에 필요한 annotation 추가

--- src/main/java/com/programmers/cat_picture_search/breed/entity/Breed.java
+++ src/main/java/com/programmers/cat_picture_search/breed/entity/Breed.java
@@ -1,12 +1,19 @@
 package com.programmers.cat_picture_search.breed.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class Breed {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String name;
--- src/main/java/com/programmers/cat_picture_search/image/entity/Image.java
+++ src/main/java/com/programmers/cat_picture_search/image/entity/Image.java
@@ -4,14 +4,21 @@ import static jakarta.persistence.FetchType.*;
 
 import com.programmers.cat_picture_search.breed.entity.Breed;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class Image {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String url;
