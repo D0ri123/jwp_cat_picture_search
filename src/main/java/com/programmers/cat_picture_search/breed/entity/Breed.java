64e5f7502bffbcaa3544dddbf053d77dc203446d
Seolhui Lee <94590894+D0ri123@users.noreply.github.com>
2023-06-29 16:04:50 +0900

    [Feat/#8] : commandLineRunner를 이용하여 API 호출 응답 데이터를 DB에 저장 (#13)
    * setting: Spring Open Feign 관련 의존성 주입
    
    * chore: api url 수정
    
    * feat: feign client 관련 config, service, 응답 dto 생성
    
    * refactor: 고유값을 string id로 설정, builder 생성
    
    * chore: 고유값 타입 변경에 따른 설정 변경
    
    * chore: dto에서 annotation setter 제거
    
    * feat: command line runner를 이용하여 api 호출 응답 데이터를 db에 저장

--- src/main/java/com/programmers/cat_picture_search/breed/entity/Breed.java
+++ src/main/java/com/programmers/cat_picture_search/breed/entity/Breed.java
@@ -1,9 +1,8 @@
 package com.programmers.cat_picture_search.breed.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,11 +12,18 @@ import lombok.NoArgsConstructor;
 public class Breed {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  private String id;
 
   private String name;
   private String temperament;
   private String origin;
+
+  @Builder
+  public Breed(String id, String name, String temperament, String origin) {
+    this.id = id;
+    this.name = name;
+    this.temperament = temperament;
+    this.origin = origin;
+  }
 
 }
