fa4f553ef655da44c4dd74331ec55e23578457a4
D0ri123 <lsh81550@gmail.com>
2023-07-03 20:32:27 +0900

    feat: 랜덤 이미지 조회하는 기능 관련 service 구현

--- src/main/java/com/programmers/cat_picture_search/image/repository/ImageRepository.java
+++ src/main/java/com/programmers/cat_picture_search/image/repository/ImageRepository.java
@@ -3,7 +3,7 @@ package com.programmers.cat_picture_search.image.repository;
 import com.programmers.cat_picture_search.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+public interface ImageRepository extends JpaRepository<Image, String>, ImageRepositoryCustom {
 
 
 }
